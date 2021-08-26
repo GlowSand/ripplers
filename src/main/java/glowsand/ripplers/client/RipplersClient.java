9cd5045db85199b95a61c1968a338e1d69e59ffd
GlowSand <theththeq@gmail.com>
2021-08-26 17:12:18 -0500

    Update Faric API

--- src/main/java/glowsand/ripplers/client/RipplersClient.java
+++ src/main/java/glowsand/ripplers/client/RipplersClient.java
@@ -6,14 +6,15 @@ import glowsand.ripplers.client.renderer.RipplerEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.impl.client.renderer.registry.EntityRendererRegistryImpl;
+import net.fabricmc.fabric.impl.client.rendering.EntityRendererRegistryImpl;
 
 @Environment(EnvType.CLIENT)
 public class RipplersClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistryImpl.INSTANCE.register(Ripplers.RIPPLER_ENTITY_TYPE, ctx ->
+
+        EntityRendererRegistryImpl.register(Ripplers.RIPPLER_ENTITY_TYPE, ctx ->
                 new RipplerEntityRenderer(ctx, new RipplerEntityModel()));
     }
 }
